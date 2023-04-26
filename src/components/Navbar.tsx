67be7de1479373b93db40823d36e63d937039ed1
ifemi27 <opatolaifeoluwav@gmail.com>
2023-04-26 14:09:14 -0700

    new logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import {FiUserPlus, FiSearch} from "react-icons/fi"
 import {CiSearch} from "react-icons/ci"
+import logo from "../assets/logo5.png"
 
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center py-4 px-4">
-        <div>LOGO.</div>
+        <div>
+          <img className="w-[90px]" src={logo} alt="" />
+        </div>
         <div className="border rounded-lg px-2 flex items-center py-2">
             <input type="search" name="search" id="search" placeholder="search"/>
             <CiSearch />
