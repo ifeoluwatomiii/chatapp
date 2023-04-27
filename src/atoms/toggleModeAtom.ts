c048ddeb0d2b028125d0ad86be7d1be02e46dd19
holladortun <holladortun.omotoso@gmail.com>
2023-04-27 03:45:01 +0100

    added post button and persistence for mode

--- src/atoms/toggleModeAtom.ts
+++ src/atoms/toggleModeAtom.ts
@@ -1,9 +1,15 @@
-import {atom} from 'recoil'
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  storage: window.localStorage,
+  key: "recoilToggleMode",
+});
 
 const toggleModeState = atom<boolean>({
-    key: 'toggleModeState', // unique ID (with respect to other atoms/selectors)
-    default: true, // default value (aka initial value)
-  });
+  key: "toggleModeState", // unique ID (with respect to other atoms/selectors)
+  default: true, // default value (aka initial value)
+  effects_UNSTABLE: [persistAtom],
+});
 
-
-  export default toggleModeState
+export default toggleModeState;
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,34 +1,44 @@
 import headshot from "../assets/headshot.jpg";
 import { TbPhotoEdit } from "react-icons/tb";
 import { RxImage } from "react-icons/rx";
+import { BsEmojiSmile } from "react-icons/bs";
+import { useState } from "react";
 
 const Post = () => {
-	return (
-		<div className="divide-y rounded-lg py-4 bg-white shadow-md dark:bg-[#15202B] dark:divide-[#38444d]">
-			<div className="px-5 py-3 ">
-				<h4 className="font-bold dark:text-white">Post Something</h4>
-			</div>
-			<div className="flex items-center justify-between gap-4 px-5 py-3">
-				<div className="w-[10%]">
-					<img
-						className="w-[50px] rounded-full h-[50px] object-cover object-top "
-						src={headshot}
-						alt=""
-					/>
-				</div>
+  const [post, setPost] = useState("");
 
-				<textarea
-					className=" w-full !outline-none max-h-[70px] min-h-[70px] dark:bg-[#273347] rounded-md px-2 pt-2 dark:text-white "
-					placeholder="What's on your mind?"
-					rows={1}
-					cols={1}></textarea>
+  return (
+    <div className="divide-y rounded-lg py-4 bg-white shadow-md dark:bg-[#15202B] dark:divide-[#38444d]">
+      <div className="px-5 py-3 ">
+        <h4 className="font-bold dark:text-white">Post Something</h4>
+      </div>
+      <div className="flex items-start justify-between gap-4 px-5 py-3">
+        <div className="w-[10%]">
+          <img
+            className="w-[50px] rounded-full h-[50px] object-cover object-top "
+            src={headshot}
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col items-end w-full gap-3">
+          <textarea
+            className=" w-full !outline-none max-h-[70px] min-h-[70px] dark:bg-[#273347] rounded-md px-2 pt-2 dark:text-white "
+            placeholder="What's on your mind?"
+            rows={1}
+            cols={1}
+          ></textarea>
+          <button className="bg-[#A060FF] text-white font-bold py-1 px-8 rounded-md">
+            POST
+          </button>
+        </div>
 
-				<div>
-					<RxImage className="text-2xl text-black/50 dark:text-white/70" />
-				</div>
-			</div>
-		</div>
-	);
+        <div className="flex flex-col gap-2">
+          <RxImage className="text-2xl cursor-pointer text-black/50 dark:text-white/70" />
+          <BsEmojiSmile className="text-2xl cursor-pointer text-black/50 dark:text-white/70" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
